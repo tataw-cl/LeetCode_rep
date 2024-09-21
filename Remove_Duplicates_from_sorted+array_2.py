48a78d57cbfa151e1f1fbff7afed1bfd4d7ae337
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2024-09-21 09:58:10 +0100

    Leetcode medium

--- Remove_Duplicates_from_sorted+array_2.py
+++ Remove_Duplicates_from_sorted+array_2.py
@@ -79,19 +79,17 @@ class Solution(object):
 
     #Other solution:
     class Solution(object):
-    def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        i = 0
-        for n in nums:
-            if i < 2 or n > nums[i - 2]:
-                nums[i] = n
-                i += 1
-        return i
+        def removeDuplicates(self, nums):
+            """
+            :type nums: List[int]
+            :rtype: int
+            """
+            i = 0
+            for n in nums:
+                if i < 2 or n > nums[i - 2]:
+                    nums[i] = n
+                    i += 1
+            return i
 
     # Time complexity: O(n)
     # Space complexity: O(1)
-
-    
---
+++ Rotate_Array.py
@@ -0,0 +1,107 @@
+
+# Code
+# Testcase
+# Test Result
+# Test Result
+# 189. Rotate Array
+# Medium
+# Topics
+# Companies
+# Hint
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4,5,6,7], k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# Example 2:
+
+# Input: nums = [-1,-100,3,99], k = 2
+# Output: [3,99,-1,-100]
+# Explanation: 
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -231 <= nums[i] <= 231 - 1
+# 0 <= k <= 105
+ 
+
+# Follow up:
+
+# Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+# Could you do it in-place with O(1) extra space?
+
+# My first Solution
+# Approach 1: Using Extra Array
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        left,right=[],[]
+        k%=len(nums)
+        for i in range((len(nums))):
+            if i <(len(nums)-k):
+                left.append(nums[i])
+            else:
+                right.append(nums[i])
+        right.extend(left)
+        for i in range(len(right)):
+            nums[i]=right[i]
+            #is very slow in time btw
+
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+# Approach 2: Using Cyclic Replacements
+# Other solution:
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        start = count = 0
+        while count < n:
+            current, prev = start, nums[start]
+            while True:
+                next_idx = (current + k) % n
+                nums[next_idx], prev = prev, nums[next_idx]
+                current = next_idx
+                count += 1
+                if start == current:
+                    break
+            start += 1
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+
+#Other solution:
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n  # Handle cases where k is greater than the length of the array
+    
+    # Rotate the array by slicing
+        nums[:] = nums[-k:] + nums[:-k]
