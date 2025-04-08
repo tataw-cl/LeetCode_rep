0c365cba4672268111cf45076f438131384787c5
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2025-04-08 23:48:19 +0100

    leetcode easy

---
+++ checkIfTwoStringsAreEquivalent.py
@@ -0,0 +1,52 @@
+# 1662. Check If Two String Arrays are Equivalent
+# Solved
+# Easy
+# Topics
+# Companies
+# Hint
+# Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+# A string is represented by an array if the array elements concatenated in order forms the string.
+
+ 
+
+# Example 1:
+
+# Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+# Output: true
+# Explanation:
+# word1 represents string "ab" + "c" -> "abc"
+# word2 represents string "a" + "bc" -> "abc"
+# The strings are the same, so return true.
+# Example 2:
+
+# Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
+# Output: false
+# Example 3:
+
+# Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
+# Output: true
+ 
+
+# Constraints:
+
+# 1 <= word1.length, word2.length <= 103
+# 1 <= word1[i].length, word2[i].length <= 103
+# 1 <= sum(word1[i].length), sum(word2[i].length) <= 103
+# word1[i] and word2[i] consist of lowercase letters.
+
+
+#My Solution:
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        """
+        :type word1: List[str]
+        :type word2: List[str]
+        :rtype: bool
+        """
+        return ''.join(word1)==''.join(word2)
+
+        #Time complexity: O(n) where n is the total number of characters in both arrays.
+        #Space complexity: O(n) where n is the total number of characters in both arrays.
+        #The space complexity is O(n) because we are storing the concatenated strings in memory.
+
