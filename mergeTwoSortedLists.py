b0d1123378070a748f00906a5dcad77948267b1d
tataw-cl <127490325+tataw-cl@users.noreply.github.com>
2025-04-16 18:19:10 +0100

    Leetcode easy

---
+++ mergeTwoSortedLists.py
@@ -0,0 +1,142 @@
+# 21. Merge Two Sorted Lists
+# Solved
+# Easy
+# Topics
+# Companies
+# You are given the heads of two sorted linked lists list1 and list2.
+
+# Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+# Return the head of the merged linked list.
+
+ 
+
+# Example 1:
+
+
+# Input: list1 = [1,2,4], list2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+# Example 2:
+
+# Input: list1 = [], list2 = []
+# Output: []
+# Example 3:
+
+# Input: list1 = [], list2 = [0]
+# Output: [0]
+ 
+
+# Constraints:
+
+# The number of nodes in both lists is in the range [0, 50].
+# -100 <= Node.val <= 100
+# Both list1 and list2 are sorted in non-decreasing order.
+
+
+#My Solution:
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy=ListNode(-1)
+        current=dummy
+        while list1 and list2:
+            if list1.val<=list2.val:
+                current.next=list1
+                list1=list1.next
+            else:
+                current.next=list2
+                list2=list2.next
+            current=current.next
+        if list1:
+            current.next=list1
+        else:
+            current.next=list2
+
+        return dummy.next
+    
+
+    #Time Complexity: O(n+m) where n is the length of list1 and m is the length of list2.
+    #Space Complexity: O(1) because we are using only a constant amount of space.
+
+
+    #Other Solutions:
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        
+        if list1.val < list2.val:
+            head = list1
+            list1 = list1.next
+        else:
+            head = list2
+            list2 = list2.next
+        
+        current = head
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                current = current.next
+                list1 = list1.next
+            else:
+                current.next = list2
+                current = current.next
+                list2 = list2.next
+        
+        if list1:
+            current.next = list1
+        else:
+            current.next = list2
+        
+        return head
+    
+
+    #Time Complexity: O(n+m) where n is the length of list1 and m is the length of list2.
+    #Space Complexity: O(1) because we are using only a constant amount of space.
+
+
+    #Other Solutions:
+    # Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next,list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1,list2.next)
+            return list2
+        
+
+        #Time Complexity: O(n+m) where n is the length of list1 and m is the length of list2.
+        #Space Complexity: O(n+m) because we are using recursion and the space used by the recursion stack is O(n+m).
