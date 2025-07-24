b4493d4465234433857b8b80d29c7cc3d0f7712b
akenjifaith1@gmail.com <127490325+tataw-cl@users.noreply.github.com>
2025-07-24 20:13:10 +0100

    Leetcode medium

---
+++ non-OverlappingIntervals.py
@@ -0,0 +1,57 @@
+# 435. Non-overlapping Intervals
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+# Note that intervals which only touch at a point are non-overlapping. For example, [1, 2] and [2, 3] are non-overlapping.
+
+ 
+
+# Example 1:
+
+# Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+# Output: 1
+# Explanation: [1,3] can be removed and the rest of the intervals are non-overlapping.
+# Example 2:
+
+# Input: intervals = [[1,2],[1,2],[1,2]]
+# Output: 2
+# Explanation: You need to remove two [1,2] to make the rest of the intervals non-overlapping.
+# Example 3:
+
+# Input: intervals = [[1,2],[2,3]]
+# Output: 0
+# Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+ 
+
+# Constraints:
+
+# 1 <= intervals.length <= 105
+# intervals[i].length == 2
+# -5 * 104 <= starti < endi <= 5 * 104
+
+
+#My Solution:
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        intervals.sort(key=lambda x: x[1])
+        
+        count = 0
+        end = float('-inf')
+        
+        for start, finish in intervals:
+            if start >= end:
+                end = finish
+                count += 1
+        
+        return len(intervals) - count
+
+        #Time Complexity: O(n)
+        #Space Complexity: O(1)
